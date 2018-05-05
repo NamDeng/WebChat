a9a5126e46855cbb01fd8cac1446506cfeae3d44
Nam Won Hyung <nwh0526@gmail.com>
2018-05-06 08:54:41 +0900

    mariadb mybatis까지 연동

--- webChatting/src/test/java/com/web/chat/MariaDBConnectionTest.java
+++ webChatting/src/test/java/com/web/chat/MariaDBConnectionTest.java
@@ -11,13 +11,13 @@ public class MariaDBConnectionTest {
 	private static final String URL = "jdbc:mariadb://127.0.0.1:3306/webchat";
 	private static final String USER = "webchat";
 	private static final String PASSWORD = "webchat123!@#";
-	
+
 	@Test
 	public void testConnection() throws Exception {
-		
+
 		Class.forName(DRIVER);
-		
-		try(Connection con = DriverManager.getConnection(URL, USER, PASSWORD)){
+
+		try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
 			System.out.println(con);
 		} catch (Exception e) {
 			e.printStackTrace();
