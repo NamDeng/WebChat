026a06c83e02603e6e00107f0047861c4a0840bd
Nam Won Hyung <nwh0526@gmail.com>
2018-05-07 01:20:41 +0900

    1. 채팅방, 사용자 DTO 추가 2. 미사용 소스 제거

--- webChatting/src/main/java/com/web/chat/controller/ChatController.java
+++ webChatting/src/main/java/com/web/chat/controller/ChatController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.web.chat.persistence.ChatRoomDAO;
 
@@ -38,8 +37,6 @@ public class ChatController {
 	@RequestMapping(value = "/chat", method = RequestMethod.GET)
 	public String moveChatRoom() {
 		
-		logger.info("count : " + dao.makeChatRoom());
-		
 		return "chat/usr.chat";
 	}
 	
@@ -49,24 +46,6 @@ public class ChatController {
 	@RequestMapping(value = "/chat", method = RequestMethod.POST)
 	public String makeChatRoom(HttpServletRequest req) {
 		
-		logger.info("/chat in post");
-		logger.info("parameter is " + req.getParameter("title"));
-		logger.info("parameter is " + req.getParameter("nickName"));
-		logger.info("parameter is " + req.getParameter("avatar"));
-		
 		return "chat/usr.chat";
-	}
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/chatting", method = RequestMethod.GET)
-	public ModelAndView chatTogether(ModelAndView model) {
-		
-		logger.info("/chatting in get");
-		model.addObject("serverTime", "asdfasdf");
-		model.setViewName("chat/usr.chat");
-		
-		return model;
 	}
 }
---
+++ webChatting/src/main/java/com/web/chat/domain/ChatRoom.java
@@ -0,0 +1,52 @@
+package com.web.chat.domain;
+
+import java.util.Date;
+
+public class ChatRoom {
+
+	private int roomId;
+	private String title;
+	private int userCount;
+	private Date regDate;
+	private Date updateDate;
+
+	public int getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(int roomId) {
+		this.roomId = roomId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getUserCount() {
+		return userCount;
+	}
+
+	public void setUserCount(int userCount) {
+		this.userCount = userCount;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+}
