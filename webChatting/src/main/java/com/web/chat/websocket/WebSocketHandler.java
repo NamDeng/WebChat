8aaf8b2cfe6d5dad386a7c7da42f6709e3b02a07
Nam Won Hyung <nwh0526@gmail.com>
2018-05-23 14:07:09 +0900

    채팅 구현 위한 라이브러리 및 초기 설정 추가

---
+++ webChatting/src/main/java/com/web/chat/websocket/WebSocketHandler.java
@@ -0,0 +1,9 @@
+package com.web.chat.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+
+	
+}
